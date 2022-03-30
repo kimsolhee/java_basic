f8ed2f34aa64439d918cb28edad812f88b10b576
kimsolhee <solsol6500@naver.com>
2022-03-31 00:15:28 +0900

    CHAPTER06 - Object-oriented Programming I Ex6_2.java 한글 깨져서 다시 commit

--- src/ch06/Ex6_2.java
+++ src/ch06/Ex6_2.java
@@ -1,15 +1,30 @@
 package ch06;
 class Ex6_2 {
 	public static void main(String args[]) {
-		Tv t1 = new Tv();  // Tv t1; t1 = new Tv();�� �� �������� ����
+		
+		/*
+		 * Q.클래스(설계도)가 왜 필요한가?
+		 * 	A.객체(제품)를 생성하기 위해서 필요.
+		 * 
+		 * Q.객체(제품)가 왜 필요한가?
+		 * 	A.객체(제품)를 사용하기 위해서 필요. 
+		 * 
+		 * Q.객체(제품)를 사용한다는 것은?
+		 * 	A.객체(제품)가 가진 속성(변수)과 기능(메서드)을 사용하기 위해.
+		 * 
+		 * 
+		 */
+		
+		Tv t1 = new Tv();  // Tv t1; t1 = new Tv();를 한문장으로 간으
 		Tv t2 = new Tv();
-		System.out.println("t1�� channel���� " + t1.channel + "�Դϴ�.");
-		System.out.println("t2�� channel���� " + t2.channel + "�Դϴ�.");
+		
+		System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
+		System.out.println("t2의 channel값은  " + t2.channel + "입니다.");
 
-		t1.channel = 7;    // channel ���� 7���� �Ѵ�.
-		System.out.println("t1�� channel���� 7�� �����Ͽ����ϴ�.");
+		t1.channel = 7;    // channel 값을 7으로 한다.
+		System.out.println("t1의 channel 값을 7로 면경하였습니다.");
 
-		System.out.println("t1�� channel���� " + t1.channel + "�Դϴ�.");
-		System.out.println("t2�� channel���� " + t2.channel + "�Դϴ�.");
+		System.out.println("t1의 channel값은  " + t1.channel + "입니다.");
+		System.out.println("t2의 channel값은  " + t2.channel + "입니다.");
 	}
 }
