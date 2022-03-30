7876e848787f846ec974dddcf8623ec94dddde37
kimsolhee <solsol6500@naver.com>
2022-03-30 23:58:57 +0900

    CHAPTER06 - Object-oriented Programming I

---
+++ src/ch06/Ex6_2.java
@@ -0,0 +1,15 @@
+package ch06;
+class Ex6_2 {
+	public static void main(String args[]) {
+		Tv t1 = new Tv();  // Tv t1; t1 = new Tv();�� �� �������� ����
+		Tv t2 = new Tv();
+		System.out.println("t1�� channel���� " + t1.channel + "�Դϴ�.");
+		System.out.println("t2�� channel���� " + t2.channel + "�Դϴ�.");
+
+		t1.channel = 7;    // channel ���� 7���� �Ѵ�.
+		System.out.println("t1�� channel���� 7�� �����Ͽ����ϴ�.");
+
+		System.out.println("t1�� channel���� " + t1.channel + "�Դϴ�.");
+		System.out.println("t2�� channel���� " + t2.channel + "�Դϴ�.");
+	}
+}
---
+++ src/ch06/Hello2.java
@@ -0,0 +1,7 @@
+package ch06;
+
+// 하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직 하다.
+public class Hello2 {} // 소스파일의 이름은 public class이름과 일치해야한다.
+	class Hello3{} // 하나의 소스파일에는 하나의 public class만 허용한다.	
+	class Hello4{} 
+
